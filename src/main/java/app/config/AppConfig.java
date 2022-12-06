a32d1a9b91a86335c37c8f411b97f02ea3a314da
Natalia Selemeneva <106182140+Selemeneva@users.noreply.github.com>
2022-12-06 17:44:55 +0300

    Initial commit

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,25 @@
+package app.config;
+
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+    @Bean(name = "dog")
+    public Dog getDog() {
+        Dog dog = new Dog();
+        dog.toString();
+        return dog;
+    }
+
+    @Bean(name = "timer")
+    public Timer getTime() {
+        Timer timer = new Timer();
+        timer.getTime();
+        return timer;
+    }
+}
